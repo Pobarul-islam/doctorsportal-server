bcacbdbe834e8e361c4af2d48d6d32e8c97bad02
Pobarul-islam <pobarul3@gmail.com>
2022-10-06 16:13:27 +0600

    user send backend

--- index.js
+++ index.js
@@ -36,16 +36,21 @@ async function run() {
       res.send(services);
     });
 
-    app.put('/user/:email', async (req, res) => {
+    app.put("/user/:email", async (req, res) => {
       const email = req.params.email;
+      const user = req.body;
       const filter = { email: email };
       const options = { upsert: true };
-       const updateDoc = {
-         $set: user,
+      const updateDoc = {
+        $set: user,
       };
-      const result = await usersCollection.updateOne(filter, updateDoc, options);
+      const result = await usersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
       res.send(result);
-       });
+    });
 
 
 
