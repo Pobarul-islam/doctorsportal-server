2e854d548672dc0092893fdf7c189e0f4bf84ec4
Pobarul-islam <pobarul3@gmail.com>
2022-10-09 17:09:08 +0600

    admin role setup

--- index.js
+++ index.js
@@ -56,10 +56,29 @@ async function run() {
       res.send(services);
     });
 
-    app.get('/user', async (req, res) => {
+    app.get('/user', verifyJWT, async (req, res) => {
       const users = await usersCollection.find().toArray();
       res.send(users);
     })
+
+       app.put("/user/admin/:email", async (req, res) => {
+         const email = req.params.email;
+        
+         const filter = { email: email };
+         
+         const updateDoc = {
+           $set: {role: 'admin'},
+         };
+         const result = await usersCollection.updateOne(
+           filter,
+           updateDoc,
+           
+         );
+
+       
+         res.send(result);
+       });
+    
 
     app.put("/user/:email", async (req, res) => {
       const email = req.params.email;
@@ -80,7 +99,7 @@ async function run() {
         process.env.ACCESS_TOKEN_SECRET,
         { expiresIn: "1h" }
       );
-      res.send({ result, accessToken: token });
+      res.send({ result, token });
     });
 
 
