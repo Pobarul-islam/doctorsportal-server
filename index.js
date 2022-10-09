6e08aee96d8b8ae0ef4d07f08da3d11ea86c3802
Pobarul-islam <pobarul3@gmail.com>
2022-10-09 15:04:35 +0600

    jwt token error solved

--- index.js
+++ index.js
@@ -15,6 +15,25 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+
+function verifyJWT(req, res, next){
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: 'UnAuthorized access' });
+  }
+
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+   
+    if (err) {
+      return res.status(403).send({message: 'Forbidden access'})
+    }
+   
+    req.decoded = decoded;
+    next();
+ });
+}
+
 async function run() {
   try {
     await client.connect();
@@ -50,6 +69,7 @@ async function run() {
         updateDoc,
         options
       );
+
       const token = jwt.sign(
         { email: email },
         process.env.ACCESS_TOKEN_SECRET,
@@ -94,11 +114,17 @@ async function run() {
 
 
   
-    app.get("/booking", async (req, res) => {
+    app.get("/booking", verifyJWT, async (req, res) => {
       const patient = req.query.patient;
-      const query = { patient: patient };
-      const bookings = await bookingCollection.find(query).toArray();
-      res.send(bookings);
+      const decodedEmail = req.decoded.email;
+      if (patient === decodedEmail) {
+        const query = { patient: patient };
+        const bookings = await bookingCollection.find(query).toArray();
+        return res.send(bookings);
+      }
+      else {
+        return res.status(403).send({ message: 'Forbidden access' });
+      }
     });
 
 
